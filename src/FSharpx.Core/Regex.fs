1841b5c77e5f94cc43b37b718336e4eb0f4ade2c
mausch <mauricioscheffer@gmail.com>
2011-11-24 12:38:15 -0300

    regex replace with accumulator

---
+++ src/FSharpx.Core/Regex.fs
@@ -0,0 +1,15 @@
+﻿namespace FSharpx
+
+open System.Text.RegularExpressions
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Regex =
+
+    let replaceWithAcc folder state input (rx: Regex) =
+        let acc = ref state
+        let evaluator (m: Match) =
+            let newState, result = folder !acc m
+            acc := newState
+            result
+        let replacement: string = rx.Replace(input, evaluator)
+        !acc, replacement
---
+++ tests/FSharpx.Tests/RegexTests.fs
@@ -0,0 +1,13 @@
+﻿module FSharpx.RegexTests
+
+open NUnit.Framework
+open System.Text.RegularExpressions
+
+[<Test>]
+let ``replace with accumulator``() =
+    let count, r = 
+        Regex "%." 
+        |> Regex.replaceWithAcc (fun s _ -> let s = s+1 in s, sprintf "@p%d" s) 0 "values (%d, %s)"
+    Assert.AreEqual(2, count)
+    Assert.AreEqual("values (@p1, @p2)", r)
+    
