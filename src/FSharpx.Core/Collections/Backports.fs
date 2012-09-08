a1daa413a8843626db322df46a1fd63265566c46
mausch <mauricioscheffer@gmail.com>
2012-09-08 15:19:11 -0300

    Start backporting readonly interfaces

---
+++ src/FSharpx.Core/Collections/Backports.fs
@@ -0,0 +1,15 @@
+﻿namespace System.Collections.Generic
+
+type IReadOnlyCollection<'T> =
+    inherit IEnumerable<'T>
+    abstract Count: int with get
+
+type IReadOnlyList<'T> =
+    inherit IReadOnlyCollection<'T>
+    abstract Item: int -> 'T with get
+
+type IReadOnlyDictionary<'TKey, 'TValue> =
+    inherit IReadOnlyCollection<KeyValuePair<'TKey, 'TValue>>
+    abstract Item: 'TKey -> 'TValue with get
+    abstract Keys: IEnumerable<'TKey> with get
+    abstract Values: IEnumerable<'TValue> with get
