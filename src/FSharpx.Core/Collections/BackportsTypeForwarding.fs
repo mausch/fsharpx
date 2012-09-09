5e2ca4ae6933399b8999beabd26b7333ebcbd746
mausch <mauricioscheffer@gmail.com>
2012-09-09 14:01:02 -0300

    Trying to set up type forwarding. Error: "This attribute cannot be use in this version of F#"

---
+++ src/FSharpx.Core/Collections/BackportsTypeForwarding.fs
@@ -0,0 +1,9 @@
+﻿module FSharpx.Core.AssemblyInfo.Backports
+
+open System.Collections.Generic
+open System.Runtime.CompilerServices
+
+[<assembly: TypeForwardedTo(typedefof<IReadOnlyCollection<_>>)>]
+[<assembly: TypeForwardedTo(typedefof<IReadOnlyList<_>>)>]
+[<assembly: TypeForwardedTo(typedefof<IReadOnlyDictionary<_,_>>)>]
+()
